84a19c5097eb381261cf0450617f738855331f77
Rakshitha2131 <90560412+Rakshitha2131@users.noreply.github.com>
2023-07-28 15:01:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let a = document.getElementById("lightcat")
+let b = document.getElementById("On")
+function switchoff()
+{
+    a.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    b.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+}
+function switchon()
+{
+    a.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    b.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+}
